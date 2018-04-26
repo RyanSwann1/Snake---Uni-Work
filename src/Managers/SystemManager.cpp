a7be655cc8b0339d38ac32566cc9f8a98995669f
Ryan Swann <RyanSwann1992@gmail.com>
2018-04-26 09:56:07 +0100

    Added missing files.

--- src/Managers/SystemManager.cpp
+++ src/Managers/SystemManager.cpp
@@ -1,12 +1,16 @@
 #include <Managers\SystemManager.h>
 #include <Systems\SystemDrawable.h>
 #include <Systems\SystemSnake.h>
+#include <Systems\SystemPlayerController.h>
+#include <Systems\SystemMovable.h>
 
 //System Manager
-SystemManager::SystemFactory::SystemFactory()
+SystemManager::SystemFactory::SystemFactory(SystemManager& systemManager)
 {
-	registerSystem<SystemDrawable>(SystemType::Drawable);
-	registerSystem<SystemSnake>(SystemType::Snake);
+	registerSystem<SystemDrawable>(systemManager, SystemType::Drawable);
+	registerSystem<SystemSnake>(systemManager, SystemType::Snake);
+	registerSystem<SystemPlayerController>(systemManager, SystemType::PlayerController);
+	registerSystem<SystemMovable>(systemManager, SystemType::Movable);
 }
 
 std::unique_ptr<SystemBase> SystemManager::SystemFactory::getSystem(SystemType type) const
@@ -18,18 +22,26 @@ std::unique_ptr<SystemBase> SystemManager::SystemFactory::getSystem(SystemType t
 
 //System Manager
 SystemManager::SystemManager()
-	: m_systemFactory(),
-	m_systems(getSystems())
+	: m_systemFactory(*this),
+	m_systems(getSystems()),
+	m_systemMessages()
 {
 	assert(!m_systems.empty());
 }
 
-void SystemManager::update() const
+void SystemManager::addSystemMessage(const SystemMessage & systemMessage)
 {
-	for (auto& system : m_systems)
+	m_systemMessages.push_back(systemMessage);
+}
+
+void SystemManager::update()
+{
+	for (const auto& system : m_systems)
 	{
 		system->update();
 	}
+
+	handleSystemMessages();
 }
 
 void SystemManager::render(sf::RenderWindow& window) const
@@ -42,6 +54,24 @@ std::array<std::unique_ptr<SystemBase>, static_cast<size_t>(SystemType::Total)> 
 {
 	std::array<std::unique_ptr<SystemBase>, static_cast<size_t>(SystemType::Total)> systems;
 	systems[static_cast<int>(SystemType::Drawable)] = m_systemFactory.getSystem(SystemType::Drawable);
+	systems[static_cast<int>(SystemType::Snake)] = m_systemFactory.getSystem(SystemType::Snake);
+	systems[static_cast<int>(SystemType::PlayerController)] = m_systemFactory.getSystem(SystemType::PlayerController);
+	systems[static_cast<int>(SystemType::Movable)] = m_systemFactory.getSystem(SystemType::Movable);
 
 	return systems;
+}
+
+void SystemManager::handleSystemMessages()
+{
+	for (const auto& systemMessage : m_systemMessages)
+	{
+		sendSystemMessage(systemMessage);
+	}
+
+	m_systemMessages.clear();
+}
+
+void SystemManager::sendSystemMessage(const SystemMessage & systemMessage) const
+{
+	m_systems[static_cast<int>(systemMessage.m_systemType)].get()->onSystemMessage(systemMessage);
 }
